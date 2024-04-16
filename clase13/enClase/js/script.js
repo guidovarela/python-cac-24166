a45755e9d15d9b89c37ef91e81d5da37c3f9b295
guidovarela <guido.varela@gmail.com>
2024-04-16 13:37:52 -0300

    clase 13 - GIT - netlify - intro JS

---
+++ clase13/enClase/js/script.js
@@ -0,0 +1,10 @@
+// variable -> var o let
+        // constantes -> const
+
+        let saludo = "chau mundo!"        
+        alert(saludo)
+        
+        saludo = 23
+        console.log(saludo)
+
+        const pi = 3.14159  
