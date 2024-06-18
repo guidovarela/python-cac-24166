be27e7846bbd2bf0bf271c0680211f12b40ba4a9
guidovarela <guido.varela@gmail.com>
2024-06-18 14:38:24 -0300

    clase 31 - Python + Flask + HTML

--- clase31-crud/app/app.py
+++ clase31-crud/app/app.py
@@ -1,15 +1,14 @@
 from flask import Flask,render_template
+from dataUser import users
 
 app = Flask(__name__)
-
-
 
 # home
 @app.route("/")
 def hello_world():
     title = "Hola Mundo!"
     # return render_template("plantilla.html",datos)
-    return render_template('index.html',titulo=title)
+    return render_template('index.html',titulo=title,users=users)
 
 @app.route('/contacto')
 def dataContacto():
---
+++ clase31-crud/app/dataUser.py
@@ -0,0 +1,36 @@
+users = [{
+            "id":"0",
+            "nombre":"Marcelo Perez",
+            "puesto":"Product Manager",
+            "linkedin":"user",
+            "email":"marcelo@sitio.com"
+        },
+        {
+            "id":"1",
+            "nombre":"David Martinez",
+            "puesto":"Sr. Developer",
+            "linkedin":"user",
+            "email":"david@sitio.com"
+        },
+        {
+            "id":"2",
+            "nombre":"Maria Ferreyra",
+            "puesto":"Cuentas",
+            "linkedin":"user",
+            "email":"maria@sitio.com"
+        },{
+            "id":"3",
+            "nombre":"Julieta Mirales",
+            "puesto":"Ux UI",
+            "linkedin":"user",
+            "email":"julieta@sitio.com"
+        },
+        {
+            "id":"4",
+            "nombre":"Maximo Argani",
+            "puesto":"Dev Ops",
+            "linkedin":"user",
+            "email":"maximo@sitio.com"
+        }
+    ]
+
