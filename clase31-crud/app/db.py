82bddf4909acf566b33d52c7122ab825d7fe3e93
guidovarela <guido.varela@gmail.com>
2024-07-02 11:56:51 -0300

    clase 34 - deploy app

--- clase31-crud/app/db.py
+++ clase31-crud/app/db.py
@@ -1,12 +1,21 @@
 # pip install pymysql
 # pip freeze > requirements.txt
+# pip install -r requirements.txt
 # conectar bbdd -> host, user, pass, db
+
 import pymysql
 
-host = "localhost"
-user = "root"
-clave= ""
-db="tienda_py"
+# local -> wamp
+# host = "localhost"
+# user = "root"
+# clave= ""
+# db="tienda_py"
+
+# remota -> pythonenaywhere
+host = "guidovarela.mysql.pythonanywhere-services.com"
+user = "guidovarela"
+clave= "codo2024"
+db="guidovarela$tienda-py"
 
 def conexionMySQL():
     return pymysql.connect(host=host,user=user,password=clave,database=db)
